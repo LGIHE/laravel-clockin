099ae28517acb5be9636f4e23258a0a2808c10e3
Caleb Nkunze <nkunzecaleb@gmail.com>
2025-10-17 11:27:45 +0300

    Update clockin email temlate

--- resources/views/emails/clockin-reminder.blade.php
+++ resources/views/emails/clockin-reminder.blade.php
@@ -5,9 +5,9 @@ Hello **{{ $userName }}**,
 
 This is your friendly reminder to clock in for today, **{{ $currentDate }}**.
 
-Please remember to clock in at your designated time: **{{ $clockinTime }}**
+Please remember to clock in for the tasks you have for the day.
 
-<x-mail::button :url="url('/attendance')">
+<x-mail::button :url="url('/')">
 Clock In Now
 </x-mail::button>
 
@@ -17,8 +17,9 @@ Clock In Now
 - Clock in on time to ensure accurate attendance records
 - Don't forget to clock out at the end of your shift
 - Contact your supervisor if you have any attendance issues
+- Contact support if you need assistance with the system
 
-**Automated message sent by the Attendance Management System**
+**Automated message sent by the Clockin System**
 
 Thanks,<br>
 {{ config('app.name') }}
